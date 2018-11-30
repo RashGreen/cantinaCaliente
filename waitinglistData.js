037bab19e6e53668899f917e358cacd1f2406a3e
Primo <bpacana@gmail.com>
2018-11-30 13:09:21 -0800

    First Commit

---
+++ waitinglistData.js
@@ -0,0 +1,12 @@
+var waitingArray = [
+    {
+      customerName: "Boris Yeltsin",
+      customerEmail: "boris@russia.com",
+      phoneNumber: "818-555-1212",
+      customerID: "BorisRocks"
+    }
+  ];
+  
+  
+  //Export array.
+  module.exports = waitingArray;
